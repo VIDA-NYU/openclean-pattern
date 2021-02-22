f66ddb9006149403662d4a2a7b2476d7b879732a
Heiko Mueller <heiko.muller@gmail.com>
2021-02-22 17:09:38 -0500

    Change openclean dependency to openclean-core

--- setup.py
+++ setup.py
@@ -13,10 +13,10 @@ from setuptools import setup, find_packages
 
 # todo: add
 install_requires = [
-    'pandas == 1.1.4',
-    'datamart_geo == 0.2',
-    'pygtrie == 2.3.3',
-    'openclean == 0.1.0'
+    'pandas==1.1.4',
+    'datamart_geo==0.2',
+    'pygtrie==2.3.3',
+    'openclean-core==0.1.0'
 ]
 
 # todo: add
